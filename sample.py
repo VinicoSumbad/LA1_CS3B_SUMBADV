5ea7c06fc4009ce129f6b6b2493d3c4678f27312
Vinico Sumbad <vinicowinnhsumbad@gmail.com>
2024-10-12 08:25:30 +0800

    initial commit

---
+++ sample.py
@@ -0,0 +1,40 @@
+print ("Hello World")
+
+if 5 > 2:
+    print("Five is greater than two")
+
+age = 20
+fullname = "Vinico Winn A. Sumbad"
+
+print(age, fullname)
+
+#This is  a comment
+
+x = 4       # x is a type of int
+x = "Sally" # x is now type of str
+print(X)
+
+#TYPE CASTING
+x = str(3)  # x will be '3'
+y = int(3)  # y will be 3
+z = float(3) # z will be 3.0
+
+x = 3
+y = 3
+z = 3
+
+print(type{x})
+print(type{y})
+print(type{z})
+
+#Output will be 3, because there is no casting
+
+#Single or Double
+#Case Sensitive
+a = "Vinico"
+A = "Sumbad"
+
+print(a)
+print(A)
+
+#Out put will be Vinico Sumbad, because "A" will not overwrite "a", thats because Python is case sensitive
